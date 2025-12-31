f4dcb0df8c0c4d711511bcaa2495c39ceb7081fc
MadouseiXalisphera <almulkanmarssya@gmail.com>
2025-12-31 08:05:38 +0700

    Overall Udah jadi sih, cuma kalau tombol on of nya display aja. gak bener bener mati nyala in fungsi. bentaran dah gw edit lagi

--- buildDrumMachine/script.js
+++ buildDrumMachine/script.js
@@ -1,8 +1,22 @@
 const drumButton = document.querySelectorAll(".drum-pad");
+const display = document.querySelector("#display");
+
+
+const bankSwitch = document.querySelector("#bank-switch");
+const outputBank = document.querySelector("#output-bank");
+outputBank.textContent="Drum"
+
+const powerSwitch = document.querySelector("#power-switch");
+const outputPower = document.querySelector("#output-power");
+outputPower.textContent="OFF"
+
+const volume = document.querySelector("#volume-value");
+let globalVolume = 0.3;
 
 function playMusic (keys) {
     const huruf = keys.key;
     const btnId = keys;
+    
     let para ;
     if(huruf){
          para = huruf.toUpperCase();
@@ -10,13 +24,46 @@ function playMusic (keys) {
          para = btnId.toUpperCase().trim();
     }
     const audio = document.getElementById(para);
-    console.log(para)
+    const textTampilan = audio.parentElement.id;
     if (audio){
         audio.currentTime = 0;
         audio.play();
+        audio.volume = globalVolume;
+        display.textContent = textTampilan.replace(/-|_/g," ")
+
     }
-    
 }
+
+function onOf(){
+    if(powerSwitch.classList.contains("active")){
+        powerSwitch.classList.remove("active");
+        outputPower.textContent="OFF"
+    }else {
+        powerSwitch.classList.add("active");
+        outputPower.textContent="ON"
+    }
+        
+}
+
+function changeMode(){
+    if(bankSwitch.classList.contains("active")){
+        bankSwitch.classList.remove("active");
+        outputBank.textContent="Drum"
+    }else {
+        bankSwitch.classList.add("active");
+        outputBank.textContent="Piano"
+    }
+
+        
+}
+
+function displayVol (val){
+    const pembulatan = Math.floor(val.target.value*100)
+    globalVolume = val.target.value
+    display.textContent= pembulatan;
+}
+
+
 drumButton.forEach(btn=>
     btn.addEventListener("click",(e)=>{
         playMusic(btn.textContent)}
@@ -24,4 +71,16 @@ drumButton.forEach(btn=>
 )
 document.addEventListener("keydown",(e)=>{
     playMusic(e);
+})
+
+powerSwitch.addEventListener("click",()=>{
+   onOf()
+})
+
+bankSwitch.addEventListener("click",()=>{
+   changeMode()
+})
+
+volume.addEventListener("click",(e)=>{
+    displayVol(e);
 })
